550cd3ed9577fd9753af039ac93ae615477645d9
mouwaficbdr <badaroumouwafic@gmail.com>
2026-04-07 08:21:41 +0100

    Registers drifters routes

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, {type Application, type Request, type Response} from "express"
 import healthRoutes from "./modules/health/health.routes.js"
 import bastionRoutes from "./modules/bastions/bastions.routes.js"
+import driftersRoutes from "./modules/drifters/driters.routes.js"
 
 const app: Application = express()
 
@@ -8,7 +9,7 @@ app.use(express.json())
 
 app.use("/api/v1/health", healthRoutes)
 app.use("/api/v1/bastions", bastionRoutes)
-
+app.use("/api/v1/drifters", driftersRoutes)
 
 export {app}
 export default app
